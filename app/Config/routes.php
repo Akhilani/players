a35ec30119455c888d1f94963bd8c472cda5a8ca
Akhil Kadangode <akhil.kadangode@gmail.com>
2014-12-20 19:48:00 +0530

    routing and menu changes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -24,7 +24,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'team', 'action' => 'index')); // route to change webroot
+	Router::connect('/', array('controller' => 'player', 'action' => 'index')); // route to change webroot
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
